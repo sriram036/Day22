35ad28b22ad5d1424dd8efff62a53573c84f7261
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-05-19 17:01:52 +0530

    first commit

---
+++ src/com/bridgelabz/AddressBookSystem.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        addressBook.getData();
+        System.out.println(addressBook);
+    }
+}
